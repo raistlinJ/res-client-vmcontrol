6194fda1035cadb7ddbfcb5f1ec9a7bb616d39cb
RaistlinJ <raistlinJ@users.noreply.github.com>
2025-02-24 21:53:45 -0700

    updated to contain res client code

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify, render_template
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
-app = Flask(__name__)
+import logging
+import time
+from engine.Manager.ExperimentManage.ExperimentManageVBox import ExperimentManageVBox
+from engine.Engine import Engine
+from engine.Configuration.UserPool import UserPool
 
+app = Flask(__name__)
 limiter = Limiter(
     get_remote_address,
     app=app,
@@ -12,23 +16,97 @@ limiter = Limiter(
     storage_uri="memory://",
 )
 
-@app.route("/slow")
-@limiter.limit("1 per day")
-def slow():
-    return ":("
-
-@app.route("/medium")
+# Route to serve the HTML page (client)
+@app.route('/')
 @limiter.limit("1/second", override_defaults=False)
-def medium():
-    return ":|"
+def index():
+    return render_template('index.html')
 
+# Route to run a command
+@app.route('/run_command', methods=['POST', 'OPTIONS'])
+@limiter.limit("1 per 30 minutes")
+def run_command():
+    # Get the password and configname from the request
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    configname = data.get('configname')
+    command = data.get('command')
+    
+    # Check if configname is provided
+    if not configname:
+        return jsonify({"error": "No Scenario Name Provided"}), 400
 
-@app.route("/fast")
-def fast():
-    return ":)"
+    try:
+        # Call the start vm command
+        userpool = UserPool()
+        creds_file = ""
+        usersConns = userpool.generateUsersConns(configname, creds_file=creds_file)
+        if (username, password) in usersConns:
+            conn = usersConns[(username, password)][0]
+            cloneVMName = conn[0]
+        else:
+            return jsonify({"error": "Invalid username or password"}), 403
+        
+        #####---Start Experiment Test#####
+        ##Note that any guestcontrol operations will require guest additions to be installed on the VM
+        logging.info("Starting Experiment")
+        e = Engine()
+        cmds = []
+        if command == "start":
+            cmds.append("experiment start malwarecomms vm " + str(cloneVMName))
+            for cmd in cmds:
+                e.execute(cmd)
+                res = e.execute("experiment status")
+                logging.debug("Waiting for experiment start to complete...")
+                while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
+                    time.sleep(.1)
+                    logging.debug("Waiting for experiment start to complete...")
+                    res = e.execute("experiment status")
+            output = "Completed"
+            output += "\n" + str(res)
 
+        elif command == "stop":
+            cmds.append("experiment stop malwarecomms vm " + str(cloneVMName))
+            for cmd in cmds:
+                e.execute(cmd)
+                res = e.execute("vm-manage mgrstatus")
+                logging.debug("Waiting for experiment start to complete...")
+                while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
+                    time.sleep(.1)
+                    logging.debug("Waiting for experiment start to complete...")
+                    res = e.execute("experiment status")
+            output = "Completed"
+            output += "\n" + str(res)
 
-@app.route("/ping")
-@limiter.exempt
-def ping():
-    return "PONG"
+        elif command == "status":
+            cmds.append("vm-manage refresh " + str(cloneVMName))
+            cmds.append("vm-manage vmstatus " + str(cloneVMName))
+            for cmd in cmds:
+                e.execute(cmd)
+                res = e.execute("vm-manage mgrstatus")
+                logging.debug("Waiting for experiment start to complete...")
+                while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
+                    time.sleep(.1)
+                    logging.debug("Waiting for experiment start to complete...")
+                    res = e.execute("vm-manage mgrstatus")
+            output = "Completed"
+            output += "\n" + str(res)
+                    
+        elif command == "restore":
+            cmds.append("experiment restore malwarecomms vm " + str(cloneVMName))
+            for cmd in cmds:
+                e.execute(cmd)
+                res = e.execute("experiment status")
+                logging.debug("Waiting for experiment start to complete...")
+                while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
+                    time.sleep(.1)
+                    logging.debug("Waiting for experiment start to complete...")
+                    res = e.execute("experiment status")
+            output = "Completed"
+            output += "\n" + str(res)
+        
+        return jsonify({"output": output}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
